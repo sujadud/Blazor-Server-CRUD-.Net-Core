54d433e88673ce87a24201db3cf413a925cf4082
Sujad Ud Doula <sujad.ud@outlook.com>
2024-05-28 22:18:31 +0600

    Added project added nuget packages

---
+++ BlazorServerCRUD/BlazorServerCRUD/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorServerCRUD
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
